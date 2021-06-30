14ab789528cfddc4744acd504490e56ee32a4876
distorition <85959781+distorition@users.noreply.github.com>
2021-06-30 21:10:02 +0300

    Add files via upload

---
+++ Lesson 5 question 2/Lesson 5 question 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5_question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+            
+            File.WriteAllText(filename, DateTime.Now.ToString());
+            string filetext = File.ReadAllText(filename);
+            Console.WriteLine(filetext);
+        }
+        
+    }
+}
