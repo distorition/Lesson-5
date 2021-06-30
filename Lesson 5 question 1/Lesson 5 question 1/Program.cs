f32ee009b8f6dbdff0d1c324566b43d4a01e1196
distorition <85959781+distorition@users.noreply.github.com>
2021-06-30 21:11:05 +0300

    Add files via upload

---
+++ Lesson 5 question 1/Lesson 5 question 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Lesson_5_question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввидите что нибудь ");
+            string a = Console.ReadLine();
+            Console.WriteLine("и еще");
+            string d = Console.ReadLine();
+            Console.WriteLine("еще больше");
+            string c = Console.ReadLine();
+            
+            
+            File.AppendAllText(a, Environment.NewLine);
+            File.AppendAllLines(a, new[] { d,c });
+
+            string sum1 = File.ReadAllText(a);
+            Console.WriteLine(sum1);
+            string[] sum = File.ReadAllLines(a);
+            Console.WriteLine(sum[2]);
+
+
+
+        }
+    }
+}
